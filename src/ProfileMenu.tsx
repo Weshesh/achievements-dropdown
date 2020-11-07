3b154b52ceacaa7432837c194269c5d3c89f28b2
Weshesh <andi.angel@boheemia.ee>
2020-11-07 16:53:53 +0200

    Remove CRA App.tsx content, default button focus outline, turn Profile menu into button

--- src/ProfileMenu.tsx
+++ src/ProfileMenu.tsx
@@ -3,13 +3,19 @@ import { Company } from './App';
 
 function ProfileMenu(props: { company: Company, currentlySelected: string}) {
   return (
-    <div className='flex px-px'>
+    <button type='button' className='flex px-px'>
       <div className='flex flex-col text-right p-px'>
-        <span className='text-sm font-bold leading-tight'>{props.company.user}</span>
-        <span className='text-sm font-light text-gray-500 leading-snug'>{props.currentlySelected}</span>
+        <span className='text-sm font-extrabold leading-tight'>
+          {props.company.user}
+        </span>
+        <span className='text-sm text-gray-500 leading-snug'>
+          {props.currentlySelected}
+        </span>
       </div>
-      <i className='icon-20 icon__cog bg-red-500'></i>
-    </div>
+      <div className='flex items-center pl-3 h-full'>
+        <i className='icon-20 icon__cog bg-gray-900'></i>
+      </div>
+    </button>
   )
 }
 
